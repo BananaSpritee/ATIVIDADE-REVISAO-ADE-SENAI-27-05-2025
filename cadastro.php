bc1981f99e056cccd0b4c67a2b00a68f54acc81f
BananaSpritee <164941682+BananaSpritee@users.noreply.github.com>
2025-05-28 19:17:05 -0300

    .

--- cadastro.php
+++ cadastro.php
@@ -53,62 +53,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="icon" href="./assets/favicon.ico" type="image/x-icon">
 </head>
 
-<body class="login-container">
+<body class="pagina-cadastro">
 
-    <main>
+    <main class="login-container">
 
-        <form class="formulario-login" method="post" action="">
+        <form class="formulario-cadastro" method="post" action="">
 
             <h2 style="text-align:center; margin-bottom:1rem;">Criar Conta</h2>
 
             <?php if ($erro): ?>
-
                 <p style="color:red;"><?= htmlspecialchars($erro) ?></p>
-
             <?php endif; ?>
 
             <?php if ($sucesso): ?>
-
                 <p style="color:green;"><?= htmlspecialchars($sucesso) ?></p>
-
                 <div class="link-cadastro">
-
                     <a href="./index.php">Fazer Login</a>
-
                 </div>
-
             <?php endif; ?>
 
+            <!-- 🔹 DUAS COLUNAS -->
             <div class="formulario-duas-colunas">
-
+                <!-- Coluna 1 -->
                 <div class="coluna-formulario">
-
                     <label for="name">Nome:</label>
                     <input type="text" name="name" id="name" required value="<?= htmlspecialchars($_POST['name'] ?? '') ?>">
 
                     <label for="email">E-mail:</label>
                     <input type="email" name="email" id="email" required value="<?= htmlspecialchars($_POST['email'] ?? '') ?>">
-
                 </div>
 
+                <!-- Coluna 2 -->
                 <div class="coluna-formulario">
-
                     <label for="password">Senha:</label>
                     <input type="password" name="password" id="password" required>
 
                     <label for="password2">Repita a senha:</label>
                     <input type="password" name="password2" id="password2" required>
-
                 </div>
-
             </div>
 
             <button type="submit">Cadastrar-se</button>
 
             <div class="link-cadastro">
-                
                 <a href="./index.php">Já tenho uma conta</a>
-
             </div>
 
         </form>
